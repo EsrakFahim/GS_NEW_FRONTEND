2760448e9b592bda4703dd5775b56d3f60b39fb6
EsrakFahim <efahim0656@gmail.com>
2025-01-01 15:00:03 +0600

    Refactor services page and update SidebarNav links for improved navigation

---
+++ src/app/admin/services/page.js
@@ -0,0 +1,15 @@
+import { AllProjects } from '@/app/admin_componensts/pages/AllProjects'
+import React from 'react'
+
+const page = () => {
+      return (
+            // <AllProjects />
+            <div>
+                  <h1>
+                        Services
+                  </h1>
+            </div>
+      )
+}
+
+export default page
